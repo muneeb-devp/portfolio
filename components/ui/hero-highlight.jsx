2b505730778b51124d655d619a03d48cd5f033d6
Muneeb Mughal <muneeb.devp@gmail.com>
2024-11-27 14:03:41 +0500

    Added id for about component

--- components/ui/hero-highlight.jsx
+++ components/ui/hero-highlight.jsx
@@ -20,6 +20,7 @@ export const HeroHighlight = ({ children, className, containerClassName }) => {
         'relative h-[40rem] flex items-center bg-white dark:bg-black justify-center w-full group',
         containerClassName
       )}
+      id="about"
       onMouseMove={handleMouseMove}
     >
       <div className="absolute inset-0 bg-dot-thick-neutral-300 dark:bg-dot-thick-neutral-800  pointer-events-none" />
